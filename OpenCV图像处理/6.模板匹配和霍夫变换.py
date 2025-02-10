d5ffbcaf9d645d29d7b088f61d667bc1ebe5b2ec
Return-Log <chnlmr@outlook.com>
2025-02-11 00:00:19 +0800

    模板匹配霍夫变换

--- OpenCV图像处理/6.模板匹配和霍夫变换.py
+++ OpenCV图像处理/6.模板匹配和霍夫变换.py
@@ -36,9 +36,9 @@ import matplotlib.pyplot as plt
 star = cv.imread("circle.jpg")
 gray = cv.cvtColor(star,cv.COLOR_BGR2GRAY)
 img = cv.medianBlur(gray,7)
-circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,200,param1=100,param2=50,minRadius=0,maxRadius=0)
-circles = circles[0, :].astype(int)
-for i in circles:
+circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,80,param1=100,param2=50,minRadius=0,maxRadius=0)
+
+for i in circles[0,:]:
     cv.circle(star,(i[0],i[1]),i[2],(0,255,0),2)
     cv.circle(star,(i[0], i[1]), 2, (255, 0, 0), -1)
 plt.imshow(star[:,:,::-1])
